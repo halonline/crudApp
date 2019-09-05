0cf36ceee79d5802bcd98b8ce5eea6855e9a023b
Thirumaleshwar <34562216+Thirumaleshwar@users.noreply.github.com>
2019-09-05 13:10:20 -0400

    Update DBConnectionUtility.java

--- src/main/java/com/app/util/DBConnectionUtility.java
+++ src/main/java/com/app/util/DBConnectionUtility.java
@@ -8,7 +8,7 @@ public class DBConnectionUtility {
 
 	public static Connection getDBConnection() {
 
-		String DATASOURCE_CONTEXT = "java:comp/env/jdbc/crudDB";
+		String DATASOURCE_CONTEXT = "java:comp/env/jdbc/crudApp";
 
 		Connection result = null;
 		try {
